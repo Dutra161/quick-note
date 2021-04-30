b810e0f7ab5acf2131c8858f350b9110d99407c8
Dutra <gabriel.d.o@hotmail.com.br>
2021-04-30 10:58:41 -0300

    HTML CSS

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,25 @@
 import React from "react";
+import {Form,Button} from "react-bootstrap";
+import "./Login.css";
 
 function Login(){
     return(
-        <dv>
-            <h1>Login</h1>
-        </dv>
+        <div className="base">
+            <div className="container">
+                <div>
+                    <img src="/imagens/Quick_note.png" alt="Quick"/>
+                    <Form className="inputs">
+                        <Form.Group controlId="email">
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+                        <Form.Group controlId="senha">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Button variant="primary">Login</Button>{' '}
+                    </Form>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Login;
